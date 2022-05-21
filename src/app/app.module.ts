99592a19f422360c0c947edccc527fa3e02703bb
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-21 09:35:03 -0300

    Login section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingComponent } from './shopping/shopping.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingComponent
+    ShoppingComponent,
+    LoginComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
